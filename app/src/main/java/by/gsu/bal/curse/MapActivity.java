d6952d5655e5e4f456409e8a1616feb6e954552d
Pavel Bal <balpoi000@gmail.com>
2023-05-03 17:37:24 +0300

    DUMP

--- app/src/main/java/by/gsu/bal/curse/MapActivity.java
+++ app/src/main/java/by/gsu/bal/curse/MapActivity.java
@@ -1,14 +1,32 @@
 package by.gsu.bal.curse;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.gms.maps.MapView;
 
 public class MapActivity extends AppCompatActivity {
 
+    private MapView mMapView;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+    }
+
+    private void initGoogleMap(Bundle savedInstanceState){
+        // *** IMPORTANT ***
+        // MapView requires that the Bundle you pass contain _ONLY_ MapView SDK
+        // objects or sub-Bundles.
+        Bundle mapViewBundle = null;
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY);
+        }
+
+        mMapView.onCreate(mapViewBundle);
+
+        mMapView.getMapAsync(this);
     }
 }
